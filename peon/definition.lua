aa1fc75bf9ae103ec2b9c52695c6ac90587d0588
aatack <alexanderjtatack@gmail.com>
2022-06-22 00:29:17 +0100

    Structure the module slightly better

--- peon/definition.lua
+++ peon/definition.lua
@@ -1,61 +1,68 @@
 require "globals"
 
-return {
-  Peon = class {
-    __init = function(self)
-      self.users = {}
-    end,
+local Context = class {
+  __init = function(self, timestamp, queue)
+    self.timestamp = timestamp
+    self.queue = queue
+    self.cache = {}
+  end,
 
-    use = function(self, peon, uses)
-      if uses then
-        if not peon.users[self] then
-          peon.users[self] = 0
-        end
-        peon.users[self] = peon.users[self] + 1
-      else
-        peon.users[self] = peon.users[self] - 1
-        if peon.users[self] <= 0 then
-          peon.users[self] = 0
-        end
-      end
-    end,
-
-    sync = function(self, context)
-      error "`sync` is not implemented"
-    end,
-
-    horizon = function(self, lazy)
-      return nil
-    end,
-
-    state = function(self)
-      return nil
+  diff = function(self, peon)
+    if self.cache[peon] == nil then
+      self.cache[peon] = peon:diff(self)
     end
-  },
+    return self.cache[peon]
+  end,
 
-  Context = class {
-    __init = function(self, timestamp)
-      self.timestamp = timestamp
-
-      self.cache = {}
-      self.queue = {}
-    end,
-
-    diff = function(self, peon)
-      if self.cache[peon] == nil then
-        self.cache[peon] = peon:sync(self)
+  force = function(self, peon)
+    self.cache[peon] = true
+    for user, count in pairs(peon.users) do
+      if count and count > 0 then
+        self.cache[user] = nil
+        self.queue[user] = true
       end
-      return self.cache[peon]
-    end,
-
-    force = function(self, peon)
-      self.cache[peon] = true
-      for user, count in pairs(peon.users) do
-        if count and count > 0 then
-          self.cache[user] = nil
-          self.queue[user] = true
-        end
-      end
-    end,
-  }
+    end
+  end,
 }
+
+local Peon = class {
+  __init = function(self)
+    self.users = {}
+  end,
+
+  sync = function(self, timestamp)
+    local context = Context(timestamp, { self = true })
+  end,
+
+  use = function(self, peon, uses)
+    if uses == nil then
+      uses = true
+    end
+
+    if uses then
+      if not peon.users[self] then
+        peon.users[self] = 0
+      end
+      peon.users[self] = peon.users[self] + 1
+    else
+      peon.users[self] = peon.users[self] - 1
+      if peon.users[self] <= 0 then
+        peon.users[self] = 0
+      end
+    end
+  end,
+
+  diff = function(self, context)
+    error "`diff` is not implemented"
+  end,
+
+  horizon = function(self, lazy)
+    return nil
+  end,
+
+  state = function(self)
+    return nil
+  end,
+}
+
+return { Peon = Peon, Context = Context }
